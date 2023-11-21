e2b647a800a085e8baa988c3a5bf953c90c8e54c
BrenC222 <brencdev.io@gmail.com>
2023-11-21 15:38:13 -0600

    Look at pandas API

---
+++ visualizer/graph_data.py
@@ -0,0 +1,8 @@
+#---------------------------
+# This script uses the pandas module to produce graphical representations of the files in /data
+#
+#
+#------------------------------------------
+
+
+def histogram():
