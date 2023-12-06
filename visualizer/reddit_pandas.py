4aa016bb086626faba77c62c7f4aa70d57d93741
BrenC222 <brencdev.io@gmail.com>
2023-12-06 13:27:33 -0600

    Fixed graphs again, for some reason my reddit_worker for reading json wasnt loading correctly to pandas df. SO I used pandas read_json method

--- visualizer/reddit_pandas.py
+++ visualizer/reddit_pandas.py
@@ -9,29 +9,23 @@ def get_graph(vibe_dir, plot_dir):
     file_list = os.listdir(vibe_dir)
 
     for file_name in file_list:
-        if file_name.endswith(".json"):
-            file_path = os.path.join(vibe_dir, file_name)
-            json_data = reddit_worker.read_json(file_path)
-            df = json_to_pandas(json_data)
+        try:
+            if file_name.endswith(".json"):
+                file_path = os.path.join(vibe_dir, file_name)
+                df = pd.read_json(file_path)
 
-            if df is not None:
-                vibe_count = df.iloc[0].value_counts()
-                vibe_labels = df.index.tolist()
+                if df is not None:
+                    vibe_count = df.loc[0].value_counts()
                 
-                # Reset graph
-                plt.clf()
+                    # Reset graph
+                    plt.clf()
                 
-                # Plot the data frame
-                plt.bar(vibe_labels, vibe_count.values)
-                plt.xlabel('Sentiments')
-                plt.ylabel('Count')
-                plt.title(f'{file_name.replace("reddit_r_", "").replace("comments_", "").replace("_sentiment.json", "")}')
-                plt.savefig(f'{plot_dir}/{file_name.replace(".json", ".png")}')
+                    # Plot the data frame
+                    plt.bar(vibe_count.index, vibe_count.values)
+                    plt.xlabel('Sentiments')
+                    plt.ylabel('Count')
+                    plt.title(f'{file_name.replace("reddit_r_", "").replace("comments_", "").replace("_sentiment.json", "")}')
+                    plt.savefig(f'{plot_dir}/{file_name.replace(".json", ".png")}')
 
-def json_to_pandas(json_data):
-    try:
-        df = pd.DataFrame([json_data])
-        return df
-
-    except Exception as e:
-        print(f"Error: {e}")
+        except Exception as e:
+            print(f"Error: {e}")
